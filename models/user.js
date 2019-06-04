0e77c3b339037f5f939719d908ad92e9d77ab23e
ChaoYIN <yinchao428@hotmail.com>
2019-06-04 15:03:27 +1000

    itcast-blog created

---
+++ models/user.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema ({
+  email: {
+    type: String,
+    required: true
+  },
+  nickname: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  created_time: {
+    type: Date,
+    default: Date.now
+  },
+  last_modified_time: {
+    type: Date,
+    default: Date.now
+  },
+  avatar: {
+    type: String,
+    default: '/public/img/avatar.default.png'
+  }
+
+})
+
+module.exports = mongoose.model('User', userSchema)
